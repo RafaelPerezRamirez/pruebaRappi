9902c3660d540c48768427d5cffac007be5b22dc
rafabrutaldrums <rafael@gafa.mx>
2017-04-10 15:02:33 -0500

    delete

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php include 'templates/header.php' ?>
-<?php include 'templates/main.php' ?>
-<?php include 'templates/footer.php' ?>
--- templates/menu.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <div class="menu-button">
-        <span></span>
-        <span></span>
-    </div>
-    <object data="src/images/logo.svg" type="image/svg+xml" class="logo"></object>
-</nav>
